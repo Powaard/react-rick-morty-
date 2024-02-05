e3d74677cf5186db01315b3d1383f7a58cfeeb7c
Powaard <abdullahkarakelle34@gmail.com>
2024-02-05 10:03:20 +0300

    rick and morty
    rick and morty

---
+++ react-rick-morty-main/backend/MyRickAndMortyApi/MyRickAndMortyApi/obj/Debug/net7.0/MyRickAndMortyApi.AssemblyInfo.cs
@@ -0,0 +1,14 @@
+-------------------------------------------------------------------------
+
+using System;
+using System.Reflection;
+
+[assembly: System.Reflection.AssemblyCompanyAttribute("MyRickAndMortyApi")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyProductAttribute("MyRickAndMortyApi")]
+[assembly: System.Reflection.AssemblyTitleAttribute("MyRickAndMortyApi")]
+[assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
+
+
